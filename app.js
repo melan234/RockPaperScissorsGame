6d8ca916a517d3987a366a5df11a30bc06e8c996
Melan Banvait <melanb07@gmail.com>
2022-01-18 08:05:41 -0500

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+function game(userChoice) {
+ console.log(getComputerChoice());
+}
+
+function main() {
+
+  rock_div.addEventListener('click', function() {
+    game("rock")
+  });
+  paper_div.addEventListener('click', function() {
+    game("paper")
+  });
+  scissors_div.addEventListener('click', function() {
+    game("scissors")
+  });
+}
+
+main();
